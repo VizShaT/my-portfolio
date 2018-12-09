6c1f9bd03ba94231f6e0a392c4d7fa22613929a2
kikit <kikit@users.noreply.github.com>
2018-12-10 01:55:38 +0530

    Updates

---
+++ precache-manifest.b02edfea85ee7f6bbd9e11e5033dcb35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2bb3e8d5477afa4b7c6",
+    "url": "/my-portfolio/static/js/runtime~main.f2bb3e8d.js"
+  },
+  {
+    "revision": "4a3dc59216f5d674d631",
+    "url": "/my-portfolio/static/js/main.4a3dc592.chunk.js"
+  },
+  {
+    "revision": "e084194893b58a1c5002",
+    "url": "/my-portfolio/static/js/1.e0841948.chunk.js"
+  },
+  {
+    "revision": "4a3dc59216f5d674d631",
+    "url": "/my-portfolio/static/css/main.89be3ce4.chunk.css"
+  },
+  {
+    "revision": "e084194893b58a1c5002",
+    "url": "/my-portfolio/static/css/1.f726335e.chunk.css"
+  },
+  {
+    "revision": "252e9bb70495bdeddf187864f517691b",
+    "url": "/my-portfolio/index.html"
+  }
+];
